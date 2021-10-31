1fc6e0d1c205642b702e2f5a5080fc345a2c89a7
Aldine Correia <aldine.correia@gmail.com>
2021-10-31 11:31:09 -0300

    Testa findById

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,7 @@
 package aplication;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
